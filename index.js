e7a04b98d88b8c1328ab379c0e1ba142d99fa580
bforbiggy <AndyHeCS@protonmail.com>
2022-11-04 00:39:14 -0400

    Mousing Over Changes Classname

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const width = 50;
 const height = 50;
 
+function onMouseOver(event) {
+	let pixel = event.currentTarget;
+	pixel.className = "pixel marked";
+}
+
 window.onload = () => {
 	const grid = document.getElementById("grid");
 
-	for (let w = 0; w < width; w++) {
+	for (let h = 0; h < height; h++) {
 		let row = document.createElement("div");
 		row.className = "row";
 
-		for (let h = 0; h < height; h++) {
+		for (let w = 0; w < width; w++) {
 			let pixel = document.createElement("div");
 			pixel.className = "pixel blank";
+			pixel.w = w;
+			pixel.h = h;
+
+			pixel.addEventListener('mouseover', onMouseOver);
 			row.appendChild(pixel);
 		}
 
