f575ce03cfacdc35cc85fcd67b3e5ae4f5b9abe0
bforbiggy <AndyHeCS@protonmail.com>
2022-11-04 00:42:34 -0400

    Refactor

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const width = 50;
 const height = 50;
 
+// Generates a row
+function createRow() {
+	let row = document.createElement("div");
+	row.className = "row";
+	return row;
+}
+
+// Generates a pixel
+function createPixel(w, h) {
+	let pixel = document.createElement("div");
+	pixel.className = "pixel blank";
+	pixel.w = w;
+	pixel.h = h;
+
+	pixel.addEventListener('mouseover', onMouseOver);
+	return pixel;
+}
+
+// Changes style when pixel is moused over
 function onMouseOver(event) {
 	let pixel = event.currentTarget;
 	pixel.className = "pixel marked";
@@ -9,20 +28,11 @@ function onMouseOver(event) {
 window.onload = () => {
 	const grid = document.getElementById("grid");
 
+	// Create all pixels in grid
 	for (let h = 0; h < height; h++) {
-		let row = document.createElement("div");
-		row.className = "row";
-
-		for (let w = 0; w < width; w++) {
-			let pixel = document.createElement("div");
-			pixel.className = "pixel blank";
-			pixel.w = w;
-			pixel.h = h;
-
-			pixel.addEventListener('mouseover', onMouseOver);
-			row.appendChild(pixel);
-		}
-
+		const row = createRow();
+		for (let w = 0; w < width; w++)
+			row.appendChild(createPixel(w, h));
 		grid.appendChild(row);
 	}
 }
