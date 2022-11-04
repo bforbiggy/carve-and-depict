cd0a2e9600cb9b8a1dc9a2355ea92706b688fcb3
bforbiggy <AndyHeCS@protonmail.com>
2022-11-04 00:22:29 -0400

    GRIDDY

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('script loaded');
+const width = 50;
+const height = 50;
+
+window.onload = () => {
+	const grid = document.getElementById("grid");
+
+	for (let w = 0; w < width; w++) {
+		let row = document.createElement("div");
+		row.className = "row";
+
+		for (let h = 0; h < height; h++) {
+			let pixel = document.createElement("div");
+			pixel.className = "pixel";
+			row.appendChild(pixel);
+		}
+
+		grid.appendChild(row);
+	}
+}
